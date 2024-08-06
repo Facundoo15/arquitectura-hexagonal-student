e2a90296ba2c61d7a9e2b90e14c2074151ad826f
Facundoo15 <fangulos1@upao.edu.pe>
2024-08-06 16:23:21 -0500

    feat: Proyecto inicial

---
+++ arquitectura-hex-sudent/src/main/java/com/crymuzz/arquitecturahexsudent/ArquitecturaHexSudentApplication.java
@@ -0,0 +1,13 @@
+package com.crymuzz.arquitecturahexsudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaHexSudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquitecturaHexSudentApplication.class, args);
+    }
+
+}
---
+++ arquitectura-hex-sudent/src/test/java/com/crymuzz/arquitecturahexsudent/ArquitecturaHexSudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crymuzz.arquitecturahexsudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaHexSudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
